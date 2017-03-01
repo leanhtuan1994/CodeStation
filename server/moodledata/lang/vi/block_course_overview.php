670eaa4056ad5eb1d75f164bb84dcad6a1738c64
Tuan Anh Le <leanhtuanptithcm@gmail.com>
2017-03-01 11:17:21 +0700

    Create Project Moodle 3.2.1+
    Create Project Moodle 3.2.1+

---
+++ server/moodledata/lang/vi/block_course_overview.php
@@ -0,0 +1,70 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_course_overview', language 'vi', branch 'MOODLE_32_STABLE'
+ *
+ * @package   block_course_overview
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['activityoverview'] = 'Bạn có {$a}s cần chú ý';
+$string['alwaysshowall'] = 'Luôn hiện tất cả';
+$string['collapseall'] = 'Thu hẹp tất cả danh sách khóa học';
+$string['configotherexpanded'] = 'Nếu được kích hoạt, các khóa học khác sẽ được mở rộng mặc định trừ khi được thay thế bởi tùy chỉnh người dùng.';
+$string['configpreservestates'] = 'Nếu được kích hoạt, các trạng thái thu hẹp/mở rộng được đặt bởi người dùng được lưu trữ và sử dụng ở mỗi lượt tải.';
+$string['course_overview:addinstance'] = 'Thêm khối tổng quan khóa học';
+$string['course_overview:myaddinstance'] = 'Thêm khối tổng quan khóa học vào nhà của tôi';
+$string['defaultmaxcourses'] = 'Số khóa học tối đa mặc định';
+$string['defaultmaxcoursesdesc'] = 'Số khóa học tối đa sẽ được hiển thị trên khối tổng quan khóa học, ô sẽ hiện tất cả';
+$string['expandall'] = 'Mở rộng tất cả danh sách khóa học';
+$string['forcedefaultmaxcourses'] = 'Buộc thực thi số khóa học tối đa';
+$string['forcedefaultmaxcoursesdesc'] = 'Nếu được đặt thì người dùng sẽ không thể thay đổi thiết lập cá nhân của anh ấy/cô ấy';
+$string['fullpath'] = 'Tất cả danh mục và tiểu mục';
+$string['hiddencoursecount'] = 'Bạn có {$a} khóa học ẩn';
+$string['hiddencoursecountplural'] = 'Bạn có {$a} khóa học ẩn';
+$string['hiddencoursecountwithshowall'] = 'Bạn có {$a->coursecount} khóa học ẩn ({$a->showalllink})';
+$string['hiddencoursecountwithshowallplural'] = 'Bạn có {$a->coursecount} khóa học ẩn ({$a->showalllink})';
+$string['message'] = 'tin nhắn';
+$string['messages'] = 'các tin nhắn';
+$string['moveafterhere'] = 'Chuyển khóa học {$a->movingcoursename} đến sau {$a->currentcoursename}';
+$string['movecourse'] = 'Chuyển khóa học: {$a}';
+$string['movecoursehere'] = 'Chuyển khóa học đến đây';
+$string['movetofirst'] = 'Chuyển khóa học {$a} lên đầu';
+$string['movingcourse'] = 'Bạn đang chuyển: {$a->fullname} ({$a->cancellink})';
+$string['none'] = 'Không';
+$string['numtodisplay'] = 'Số khóa học hiển thị:';
+$string['onlyparentname'] = 'Chỉ có danh mục cha';
+$string['otherexpanded'] = 'Các khóa học khác được mở rộng';
+$string['pluginname'] = 'Tổng quan khóa học';
+$string['preservestates'] = 'Giữ trạng thái được mở rộng';
+$string['shortnameprefix'] = 'Bao gồm {$a}';
+$string['shortnamesufixprural'] = '(và {$a} cái khác)';
+$string['shortnamesufixsingular'] = '(và {$a} cái khác)';
+$string['showcategories'] = 'Danh mục hiển thị';
+$string['showcategoriesdesc'] = 'Liệu các danh mục khóa học có nên được hiển thị bên dưới từng khóa học?';
+$string['showchildren'] = 'Hiện con';
+$string['showchildrendesc'] = 'Liệu các khóa học con được liệt kê dưới tiêu đề khóa học chính?';
+$string['showwelcomearea'] = 'Hiện khu chào mừng';
+$string['showwelcomeareadesc'] = 'Hiện khu chào mừng trên danh sách khóa học?';
+$string['view_edit_profile'] = '(Xem và sửa hồ sơ của bạn.)';
+$string['welcome'] = 'Chào mừng {$a}';
+$string['youhavemessages'] = 'Bạn có {$a} chưa đọc';
+$string['youhavenomessages'] = 'Bạn không có tin chưa đọc nào';
---
+++ server/moodledata/localcache/mustache/1488293212/adaptable/__Mustache_00c8ea1845becbe71d613a71a18e2fbf.php
@@ -0,0 +1,55 @@
+<?php
+
+class __Mustache_00c8ea1845becbe71d613a71a18e2fbf extends Mustache_Template
+{
+    private $lambdaHelper;
+
+    public function renderInternal(Mustache_Context $context, $indent = '')
+    {
+        $this->lambdaHelper = new Mustache_LambdaHelper($this->mustache, $context);
+        $buffer = '';
+        $blocksContext = array();
+
+        $buffer .= $indent . '<img ';
+        // 'attributes' section
+        $value = $context->find('attributes');
+        $buffer .= $this->section6805fd502f1e55bd3a63b02c625bf221($context, $indent, $value);
+        $buffer .= '/>';
+
+        return $buffer;
+    }
+
+    private function section6805fd502f1e55bd3a63b02c625bf221(Mustache_Context $context, $indent, $value)
+    {
+        $buffer = '';
+        $blocksContext = array();
+    
+        if (!is_string($value) && is_callable($value)) {
+            $source = '{{name}}="{{value}}" ';
+            $result = call_user_func($value, $source, $this->lambdaHelper);
+            if (strpos($result, '{{') === false) {
+                $buffer .= $result;
+            } else {
+                $buffer .= $this->mustache
+                    ->loadLambda((string) $result)
+                    ->renderInternal($context);
+            }
+        } elseif (!empty($value)) {
+            $values = $this->isIterable($value) ? $value : array($value);
+            foreach ($values as $value) {
+                $context->push($value);
+                
+                $value = $this->resolveValue($context->find('name'), $context);
+                $buffer .= call_user_func($this->mustache->getEscape(), $value);
+                $buffer .= '="';
+                $value = $this->resolveValue($context->find('value'), $context);
+                $buffer .= call_user_func($this->mustache->getEscape(), $value);
+                $buffer .= '" ';
+                $context->pop();
+            }
+        }
+    
+        return $buffer;
+    }
+
+}
